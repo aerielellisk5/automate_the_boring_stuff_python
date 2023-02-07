86461075d58ceffdc05cf515667a2ff39feb1ea8
Aeriel Ellis <aerielellisk5@gmail.com>
2023-02-07 14:04:42 -0800

    if_while_for_practice

---
+++ if_else_statements.py
@@ -0,0 +1,28 @@
+# name = 'Alice'
+# if name == 'Alice':
+#     print('Hi Alice')
+# print ('Done')
+
+# name = 'Bob'
+# if name == 'Alice':
+#     print('Hi Alice')
+# print ('Done')
+
+# password = 'swordfish'
+
+# if password == 'swordfish':
+#     print('Access Granted')
+# else:
+#     print('Wrong Password')
+
+name = 'Bob'
+age = 3000
+
+if name == 'Alice':
+    print('Hi Alice')
+elif age < 12:
+    print('You are not Alice kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is a mortal')
+elif age > 100
+    print('You are not Alice Grannie')
