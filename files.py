15045b3c3f09c3044626a21e63427c68f2bdfeab
Aeriel <aerielellisk5@gmail.com>
2023-03-13 14:12:26 -0700

    learning about files and the os module

---
+++ files.py
@@ -0,0 +1,22 @@
+# >>> hellofile = open('/Users/aerielellis/Desktop/hello.txt')
+# >>> hellofile.read()
+# 'hello there\n'
+# >>> content = hellofile.read()
+# >>> print(content)
+
+# >>> hellofile.read()
+# ''
+# >>> hellofile = open('/Users/aerielellis/Desktop/hello.txt')
+# >>> hellofile.read()
+# 'hello there\n'
+# >>> content = hellofile.read()
+# >>> print(content)
+
+# >>> hellofile.read()
+# ''
+# >>> hellofile = open('/Users/aerielellis/Desktop/hello.txt')
+# >>> content = hellofile.read()
+# >>> print(content)
+# hello there
+
+# confused as to why I was getting an empty string on line 8? 
