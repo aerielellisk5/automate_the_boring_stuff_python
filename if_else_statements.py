0d9ab0ca7a8516d87cdecb0a97b58c046746254d
Aeriel <aerielellisk5@gmail.com>
2023-03-21 06:51:56 -0700

    learning a little about webscraping

--- if_else_statements.py
+++ if_else_statements.py
@@ -15,14 +15,14 @@
 # else:
 #     print('Wrong Password')
 
-name = 'Bob'
-age = 3000
+# name = 'Bob'
+# age = 3000
 
-if name == 'Alice':
-    print('Hi Alice')
-elif age < 12:
-    print('You are not Alice kiddo.')
-elif age > 2000:
-    print('Unlike you, Alice is a mortal')
-elif age > 100
-    print('You are not Alice Grannie')
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif age < 12:
+#     print('You are not Alice kiddo.')
+# elif age > 2000:
+#     print('Unlike you, Alice is a mortal')
+# elif age > 100
+#     print('You are not Alice Grannie')
